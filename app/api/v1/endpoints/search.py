db8e8585b6aa434fab783ff7b0f10b54f5389e1d
nitesh <more.ni@northeastern.edu>
2024-08-17 07:06:45 -0400

    fix: fixed issue rregarding response

--- app/api/v1/endpoints/search.py
+++ app/api/v1/endpoints/search.py
@@ -1,27 +1,11 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter
+from app.core.config import config
 from app.services.query_service import query_llm
 
 router = APIRouter()
 
-# Define a Pydantic model for the request body
-class QueryRequest(BaseModel):
-    query: str
-
-@router.post("/search/")
-def vector_search(request: QueryRequest):
-    try:
-        # Extract the query from the request body
-        query = request.query
-        
-        # Call the query_llm function with the query
-        results = query_llm(query)
-        
-        # If results are not None, return them
-        if results:
-            return results
-        else:
-            raise HTTPException(status_code=500, detail="Error generating response")
-    except Exception as e:
-        # Handle unexpected errors
-        raise HTTPException(status_code=500, detail=str(e))
+@router.get("/search/")
+def vector_search(query: str):
+    results = query_llm(query)
+    # generated_response = generate_response_from_documents(results)
+    return results
